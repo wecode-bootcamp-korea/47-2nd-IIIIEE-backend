76e45f9a0a8087ac15f7ec743b00ad9af3a40a85
seongnam <jseongnam1109@gmail.com>
2023-07-14 18:08:58 +0900

    INITIAL-SETTING

---
+++ server.js
@@ -0,0 +1,26 @@
+import {createApp} from './app.js';
+import {dataSource} from './api/models/dataSource.js';
+
+const startServer = async () => {
+  const app = createApp();
+  const PORT = process.env.PORT || 3000;
+  
+  app.listen(PORT, async() => {
+    console.log(`Listening to request on 127.0.0.1:${PORT}`);
+    await dataSource
+    .initialize()
+    .then(() => {
+      console.log('Data Source has been initialized!');
+    })
+    .catch((error) => {
+      console.error('Error during Data Source initialization', error);
+    });
+  
+  });
+  app.get('/ping',(req,res) => {
+    res.json({ message: "pong" });
+  })
+};
+
+startServer();
+                                
