f1b06e660653d25f40e566f71d958e5ccf9defb3
신동현 <donghyeun02@gmail.com>
2023-07-19 15:56:37 +0900

    Modify : kakao 토큰이 아닌 인가코드를 받아오도록 수정
    Modify : 불필요한 부분 제거

--- api/services/userService.js
+++ api/services/userService.js
@@ -3,12 +3,29 @@ import axios from 'axios';
 
 import { userDao } from '../models/index.js';
 
-const kakaoLogin = async (kakaoToken) => {
+const kakaoLogin = async (kakaoCode) => {
+  const clientId = process.env.CLIENT_ID;
+  const redirectUri = process.env.REDIRECT_URI;
+
+  const kakaoToken = await axios({
+    method: 'POST',
+    url: 'https://kauth.kakao.com/oauth/token',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8',
+    },
+    data: {
+      grant_type: 'authorization_code',
+      client_id: clientId,
+      redirect_uri: redirectUri,
+      code: kakaoCode,
+    },
+  });
+
   const result = await axios({
     method: 'GET',
     url: 'https://kapi.kakao.com/v2/user/me',
     headers: {
-      Authorization: `Bearer ${kakaoToken}`,
+      Authorization: `Bearer ${kakaoToken.data.access_token}`,
       'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8',
     },
   });
