bfde3beb48b6cc7ad5c35862f664d4188e255bf4
신동현 <donghyeun02@gmail.com>
2023-07-19 12:10:37 +0900

    Add : 식당 정보 조회 기능
    Modify : 식당 id값이 없을 시 에러처리 추가
    
    Modify : query exist 삭제

--- api/models/restaurantDao.js
+++ api/models/restaurantDao.js
@@ -1,6 +1,6 @@
 import { dataSource } from './dataSource.js';
 
-const districts = async() => {
+const districts = async () => {
   try {
     return await dataSource.query(
       `
@@ -9,7 +9,7 @@ const districts = async() => {
         name
       FROM districts
       `
-    )
+    );
   } catch {
     const error = new Error('DATASOURCE_ERROR');
     error.statusCode = 400;
@@ -17,6 +17,32 @@ const districts = async() => {
   }
 };
 
-export default {
-  districts
-}
+const getRestaurantInfo = async (restaurantId) => {
+  const [restaurantInfo] = await dataSource.query(
+    `
+      SELECT 
+        re.id restaurantId,
+        re.name,
+        re.district_id districtId,
+        re.type,
+        re.rating star,
+        re.location_x x,
+        re.location_y y,
+        re.description,
+        JSON_ARRAYAGG(
+          JSON_OBJECT(
+            'id', ri.id,
+            'image', ri.image
+          )
+        ) images
+      FROM restaurants re
+      JOIN restaurant_images ri ON re.id = ri.restaurant_id
+      WHERE re.id = ?
+      `,
+    [restaurantId]
+  );
+
+  return restaurantInfo;
+};
+
+export default { districts, getRestaurantInfo };
--- api/services/restaurantService.js
+++ api/services/restaurantService.js
@@ -1,9 +1,22 @@
 import { restaurantDao } from '../models/index.js';
 
-const districts = async() => {
+const districts = async () => {
   return await restaurantDao.districts();
-}
+};
 
-export default {
-  districts
-}
+const getRestaurantInfo = async (restaurantId) => {
+  const restaurant = await restaurantDao.getRestaurantInfo(restaurantId);
+
+  const existRestaurant = restaurant.restaurantId;
+
+  if (existRestaurant === null) {
+    const error = new Error('INVALID_RESTAURANT_ID');
+    error.statusCode = 404;
+
+    throw error;
+  }
+
+  return restaurant;
+};
+
+export default { districts, getRestaurantInfo };
