192f74934d6cb67ab69800703d3a0bbb9a7daff1
seongnam <jseongnam1109@gmail.com>
2023-07-21 00:11:57 +0900

    수정 1차

--- api/routes/restaurantRouter.js
+++ api/routes/restaurantRouter.js
@@ -1,10 +1,13 @@
-import express from 'express';
-import { restaurantController } from '../controllers/index.js';
+import express from "express";
+import { restaurantController } from "../controllers/index.js";
 
 const restaurantRouter = express.Router();
 
-restaurantRouter.get('/categories/districts', restaurantController.districts);
-restaurantRouter.get('/info/:restaurantId', restaurantController.getRestaurantInfo);
-restaurantRouter.get('/restaurantList', restaurantController.restaurantList);
+restaurantRouter.get("/categories/districts", restaurantController.districts);
+restaurantRouter.get(
+  "/info/:restaurantId",
+  restaurantController.getRestaurantInfo
+);
+restaurantRouter.get("/restaurantList", restaurantController.restaurantList);
 
 export { restaurantRouter };
--- api/services/userService.js
+++ api/services/userService.js
@@ -1,21 +1,20 @@
-import jwt from 'jsonwebtoken';
-import axios from 'axios';
-import { ageRange, genderType } from '../enum/categories.js';
+import jwt from "jsonwebtoken";
+import axios from "axios";
 
-import { userDao } from '../models/index.js';
+import { userDao } from "../models/index.js";
 
 const kakaoLogin = async (kakaoCode) => {
   const clientId = process.env.CLIENT_ID;
   const redirectUri = process.env.REDIRECT_URI;
 
   const kakaoToken = await axios({
-    method: 'POST',
-    url: 'https://kauth.kakao.com/oauth/token',
+    method: "POST",
+    url: "https://kauth.kakao.com/oauth/token",
     headers: {
-      'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8',
+      "Content-Type": "application/x-www-form-urlencoded;charset=utf-8",
     },
     data: {
-      grant_type: 'authorization_code',
+      grant_type: "authorization_code",
       client_id: clientId,
       redirect_uri: redirectUri,
       code: kakaoCode,
@@ -23,11 +22,11 @@ const kakaoLogin = async (kakaoCode) => {
   });
 
   const result = await axios({
-    method: 'GET',
-    url: 'https://kapi.kakao.com/v2/user/me',
+    method: "GET",
+    url: "https://kapi.kakao.com/v2/user/me",
     headers: {
       Authorization: `Bearer ${kakaoToken.data.access_token}`,
-      'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8',
+      "Content-Type": "application/x-www-form-urlencoded;charset=utf-8",
     },
   });
 
@@ -36,6 +35,25 @@ const kakaoLogin = async (kakaoCode) => {
     properties: { nickname: name, profile_image: profileImage },
     kakao_account: { email: email, age_range: age, gender: gender },
   } = result.data;
+
+  const ageRange = Object.freeze({
+    "20~29": 1,
+    "30~39": 2,
+    "40~49": 3,
+    "50~59": 4,
+    "60~69": 5,
+    "70~79": 6,
+    "80~89": 7,
+    all: 8,
+    unknown: 9,
+  });
+
+  const genderType = Object.freeze({
+    male: 1,
+    female: 2,
+    all: 3,
+    unknown: 4,
+  });
 
   const ageId = ageRange[age] || 9;
   const genderId = genderType[gender] || 4;
