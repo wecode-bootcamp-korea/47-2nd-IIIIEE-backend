69a25a397dc579293e28f8f3e6ceac10fb73164c
seongnam <jseongnam1109@gmail.com>
2023-07-21 00:11:57 +0900

    수정 1차

--- api/controllers/index.js
+++ api/controllers/index.js
@@ -1,5 +1,11 @@
-import roomController from './roomController.js';
-import userController from './userController.js';
-import restaurantController from './restaurantController.js';
+import roomController from "./roomController.js";
+import userController from "./userController.js";
+import restaurantController from "./restaurantController.js";
+import reviewController from "./reviewController.js";
 
-export { roomController, userController, restaurantController };
+export {
+  roomController,
+  userController,
+  restaurantController,
+  reviewController,
+};
--- api/services/index.js
+++ api/services/index.js
@@ -1,5 +1,6 @@
-import roomService from '../services/roomService.js';
-import userService from './userService.js';
-import restaurantService from './restaurantService.js';
+import roomService from "../services/roomService.js";
+import userService from "./userService.js";
+import restaurantService from "./restaurantService.js";
+import reviewService from "./reviewService.js";
 
-export { roomService, userService, restaurantService };
+export { roomService, userService, restaurantService, reviewService };
