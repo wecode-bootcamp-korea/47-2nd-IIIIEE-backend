b98ead0b3a307dc4b45f9c1e53545c31195dcf73
lchoe24 <lena.choe02@gmail.com>
2023-07-20 15:56:03 +0900

    Add : 식당 정보 조회 기능

--- api/controllers/restaurantController.js
+++ api/controllers/restaurantController.js
@@ -1,5 +1,5 @@
-import { restaurantService } from '../services/index.js';
-import { catchAsync } from '../utils/error.js';
+import { restaurantService } from "../services/index.js";
+import { catchAsync } from "../utils/error.js";
 
 const districts = catchAsync(async (req, res) => {
   const districts = await restaurantService.districts();
@@ -10,15 +10,34 @@ const getRestaurantInfo = catchAsync(async (req, res) => {
   const { restaurantId } = req.params;
 
   if (!restaurantId) {
+<<<<<<< HEAD
     const error = new Error('KEY_ERROR');
+=======
+    const error = new Error("KEY_ERROR");
+>>>>>>> 179d139 (Modify: 로그인 된 유저가 호스트인 방 뺴고 유저가 신청한 방 조회하기)
     error.statusCode = 400;
 
     throw error;
   }
 
+<<<<<<< HEAD
   const getRestaurantInfo = await restaurantService.getRestaurantInfo(restaurantId);
+=======
+  const getRestaurantInfo = await restaurantService.getRestaurantInfo(
+    restaurantId
+  );
+>>>>>>> 179d139 (Modify: 로그인 된 유저가 호스트인 방 뺴고 유저가 신청한 방 조회하기)
 
   return res.status(200).json({ data: getRestaurantInfo });
 });
 
-export default { districts, getRestaurantInfo };
+const restaurantList = catchAsync(async (req, res) => {
+  const restaurantList = await restaurantService.getRestaurantList(req.query);
+  res.status(200).json({ data: restaurantList });
+});
+
+export default {
+  districts,
+  restaurantList,
+  getRestaurantInfo,
+};
--- api/routes/restaurantRouter.js
+++ api/routes/restaurantRouter.js
@@ -1,9 +1,10 @@
-import express from 'express';
-import { restaurantController } from '../controllers/index.js';
+import express from "express";
+import { restaurantController } from "../controllers/index.js";
 
 const restaurantRouter = express.Router();
 
-restaurantRouter.get('/categories/districts', restaurantController.districts);
-restaurantRouter.get('/:restaurantId', restaurantController.getRestaurantInfo);
+restaurantRouter.get("/categories/districts", restaurantController.districts);
+restaurantRouter.get("/:restaurantId", restaurantController.getRestaurantInfo);
+restaurantRouter.get("/restaurantList", restaurantController.restaurantList);
 
 export { restaurantRouter };
